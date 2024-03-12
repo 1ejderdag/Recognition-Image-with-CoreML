a8c62f33d9abec30e3545a36192d3917ce8bd701
Ejder Dag <ejdrdag@gmail.com>
2024-03-12 15:53:49 +0300

    Initial Commit

---
+++ RecognitonWithMachineLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecognitonWithMachineLearning
+//
+//  Created by Ejder Dağ on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
